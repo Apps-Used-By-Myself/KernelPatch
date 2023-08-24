6d5f7ea8b37ad11dcb6001f3c85b2dbd7a343acc
bmax121 <nullret@outlook.com>
2023-08-24 13:43:33 +0000

    x

--- kernel/module/hello/hello.c
+++ kernel/module/hello/hello.c
@@ -17,5 +17,10 @@ int init()
 {
     int a = 20;
     int b = 10;
-    logkd("a") hook() return 0;
+    logkd("before hook add(a,b)=%d\n", add(a,b));
+
+    hook((void*)add, (void*)replace_add, (void**)&backup_add);
+    
+    logkd("after hook add(a,b)=%d\n", add(a,b));
+    return 0;
 }
